3e51c434f5ec62471a71a91ca3509e1cc97d50f4
ShubhAhuja3112 <shubh.ahuja9999999@gmail.com>
2025-08-20 17:42:11 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,11 @@
+/*experiment 1 : installation,environment setup and*/
+#include<stdio.h>
+
+int main()
+{
+	int a,b;
+	printf("enter the value of a and b");
+	scanf("%d%d",&a,&b);
+	printf("the value of sum of a and b is %d\n",a+b);
+	return 0;
+}
